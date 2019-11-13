1545bcaaa49c7dd3916fca93c37b8e74bc7cf80a
maxwedwards <youwontforgetthis@gmail.com>
2019-11-13 23:28:16 +0000

    split out verack generation. Still not accepted by btcd mock peer

--- main.go
+++ main.go
@@ -59,6 +59,15 @@ func main() {
         }
 }
 
+func createVerack() []byte {
+        decoded, err := hex.DecodeString(strings.Replace("16 1c 14 12 76 65 72 73 69 6f 6e 00 00 00 00 00 64 00 00 00 35 8d 49 32 62 ea 00 00 01 00 00 00 00 00 00 00 11 b2 d0 50 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 00 00 00 00 3b 2e b3 5d 8c e6 17 65 0f 2f 53 61 74 6f 73 68 69 3a 30 2e 37 2e 32 2f c0 3e 03 00", " ", "", -1))
+	if err != nil {
+		fmt.Println("Error decoding hex:", err.Error())
+		return nil
+	}
+        return decoded
+}
+
 func handleConnection(c net.Conn) {
         fmt.Printf("Serving %s\n", c.RemoteAddr().String())
 
@@ -84,14 +93,8 @@ func handleConnection(c net.Conn) {
 
 	fmt.Printf("received header %x\n", headerbuff)
 	fmt.Printf("received %x\n", messagebuff)
-
-	decoded, err := hex.DecodeString(strings.Replace("16 1c 14 12 76 65 72 73 69 6f 6e 00 00 00 00 00 64 00 00 00 35 8d 49 32 62 ea 00 00 01 00 00 00 00 00 00 00 11 b2 d0 50 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 00 00 00 00 3b 2e b3 5d 8c e6 17 65 0f 2f 53 61 74 6f 73 68 69 3a 30 2e 37 2e 32 2f c0 3e 03 00", " ", "", -1))
-	if err != nil {
-		fmt.Println("Error decoding hex:", err.Error())
-		return
-	}
-
-	c.Write(decoded)
+	
+	c.Write(createVerack())
 
 	// time.Sleep(200 * time.Millisecond)
 
